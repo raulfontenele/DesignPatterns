c23e02b2ea51aa2c5ac5446ef3a4727f5d857b4d
Raul Fontenele <raulfontenele@edu.unifor.br>
2022-12-03 12:00:54 -0300

    Adicionado primeiro exemplo de implementação do padrão strategy, utilizando como exemplo uma calculadora de impostos.

---
+++ DesignPatterns/Strategy/Calculadora.Imposto/Concrete/CalculadoraImposto.cs
@@ -0,0 +1,12 @@
+﻿using Calculadora.Imposto.Interfaces;
+
+namespace Calculadora.Imposto.Concrete
+{
+    public class CalculadoraImposto
+    {
+        public decimal Calcular(IOrcamento orcamento, IImposto imposto)
+        {
+            return imposto.Calcular(orcamento);
+        }
+    }
+}
---
+++ DesignPatterns/Strategy/Calculadora.Imposto/Concrete/Orcamento.cs
@@ -0,0 +1,14 @@
+﻿using Calculadora.Imposto.Interfaces;
+
+namespace Calculadora.Imposto.Concrete
+{
+    public class Orcamento : IOrcamento
+    {
+        private decimal _valor;
+        public Orcamento(Decimal valor)
+        {
+            _valor = valor;
+        }
+        public decimal Valor() { return _valor; }
+    }
+}
