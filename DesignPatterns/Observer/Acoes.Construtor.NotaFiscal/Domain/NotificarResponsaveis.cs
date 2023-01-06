e55b0f3d349c4c3069beed8421f6a710842cdaa5
Raul Fontenele <raulfontenele@edu.unifor.br>
2023-01-05 21:56:49 -0300

    Adicionado o padrão Observer

---
+++ DesignPatterns/Observer/Acoes.Construtor.NotaFiscal/Domain/NotificarResponsaveis.cs
@@ -0,0 +1,13 @@
+﻿using Acoes.Construtor.NotaFiscal;
+using Acoes.Construtor.NotaFiscal.Interface;
+
+namespace Acoes.Construtor.NotaFiscal.Domain
+{
+    public class NotificarResponsaveis : IAcoesNotaFiscal
+    {
+        public void Executar(INotaFiscal notaFiscal)
+        {
+            Console.WriteLine("Notificando responsáveis ...");
+        }
+    }
+}
---
+++ DesignPatterns/Observer/Acoes.Construtor.NotaFiscal/Domain/SalvarDados.cs
@@ -0,0 +1,14 @@
+﻿using Acoes.Construtor.NotaFiscal;
+using Acoes.Construtor.NotaFiscal.Interface;
+
+namespace Acoes.Construtor.NotaFiscal.Domain
+{
+    public class SalvarDados : IAcoesNotaFiscal
+    {
+
+        public void Executar(INotaFiscal notaFiscal)
+        {
+            Console.WriteLine("Salvando dados ...");
+        }
+    }
+}
